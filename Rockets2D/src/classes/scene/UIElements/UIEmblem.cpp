63f7bb131fe4819da4a6129915a02912d157ef32
Geeoon Chung <geeoon13@gmail.com>
2020-11-28 12:43:03 -0600

    Text appears when moused over emblem, but doens't work for large font sizes.

--- Rockets2D/src/classes/scene/UIElements/UIEmblem.cpp
+++ Rockets2D/src/classes/scene/UIElements/UIEmblem.cpp
@@ -9,17 +9,21 @@ UIEmblem::UIEmblem(sf::RenderTexture* t, sf::RenderWindow* w, long double* xP, l
 	xPointer = xP;
 	yPointer = yP;
 	sprite.setPosition((float)*xP, (float)*yP);
-	//description = std::make_unique<UIString>(t, w, (int)*xP, (int)*yP);
+	description = std::make_unique<UIString>(t, w, (int)*xP, (int)*yP, titl, f, 12);
 	//title = std::make_unique<UIString>(t, w, xP, yP);
 }
 
 void UIEmblem::update() {
 	sprite.setPosition((float)*xPointer, (float)*yPointer);
-	sprite.setRadius(10 * texture->getView().getSize().x / texture->getSize().x);
+	double relSize = texture->getView().getSize().x / texture->getSize().x;
+	sprite.setRadius(10 * relSize);
 	sprite.setOrigin((float)sprite.getLocalBounds().width / 2, (float)sprite.getLocalBounds().height / 2);
-	sprite.setOutlineThickness((3 * texture->getView().getSize().x / texture->getSize().x < 1) ? 1 : (3 * texture->getView().getSize().x / texture->getSize().x));
+	sprite.setOutlineThickness((3 * relSize < 1) ? 1 : (3 * relSize));
 	texture->draw(sprite);
 	if (sprite.getGlobalBounds().contains(texture->mapPixelToCoords(sf::Mouse::getPosition(*window)))) {
-		std::cout << texture->mapPixelToCoords(sf::Mouse::getPosition(*window)).x << std::endl;
+		//std::cout << texture->mapPixelToCoords(sf::Mouse::getPosition(*window)).x << std::endl;
+		description->setPos(*xPointer, *yPointer);
+		description->setSize(150000);// error here due to large font size
+		description->update();
 	}
 }
